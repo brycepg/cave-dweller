652b5c03b9881d0867fa31b6ed7085910179be17
Bryce Guinta <bguinta@uccs.edu>
2015-12-27 06:51:28 -0700

    util import game cycle problems

--- cave_dweller/util.py
+++ cave_dweller/util.py
@@ -1,10 +1,12 @@
 import os
 
 def get_neighbors(x, y):
+    from game import Game 
     """Get taxicab neighbors(4-way) from coordinates"""
     return [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
 
 def within_bounds(x, y):
+    from game import Game 
     """Check whether local coordinates are inside its local block bounds"""
     return (0 <= x < Game.map_size and 0 <= y < Game.map_size)
 
@@ -13,4 +15,3 @@ def game_path(rel_path):
     final_path = os.path.join(base_path, rel_path)
     return final_path
 
-from game import Game 
