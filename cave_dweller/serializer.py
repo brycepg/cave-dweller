c2ff35a378688c511483b9064eec23da83dd891a
Bryce Guinta <bguinta@uccs.edu>
2015-12-30 10:23:27 -0700

    Use with keyword for serialized accessor.

--- cave_dweller/serializer.py
+++ cave_dweller/serializer.py
@@ -2,6 +2,7 @@ import shelve
 import os
 import logging
 import shutil
+from contextlib import closing
 
 from util import game_path
 from block import Block
@@ -37,14 +38,13 @@ class Serializer(object):
         """Save tiles/objects for block"""
         block_name = "block%d,%d" % (block.idx, block.idy)
         block_path = os.path.join(self.serial_path, block_name)
-        block_sh = shelve.open(block_path)
-        block_sh['tiles'] = block.tiles
-        block_sh['entities'] = block.entities
-        block_sh['hidden_map'] = block.hidden_map
-        block_sh['obstacle_map'] = block.obstacle_map
-        save_turn = block.world.turn if block.save_turn is None else block.save_turn
-        block_sh['save_turn'] = save_turn
-        block_sh.close()
+        with closing(shelve.open(block_path)) as block_sh:
+            block_sh['tiles'] = block.tiles
+            block_sh['entities'] = block.entities
+            block_sh['hidden_map'] = block.hidden_map
+            block_sh['obstacle_map'] = block.obstacle_map
+            save_turn = block.world.turn if block.save_turn is None else block.save_turn
+            block_sh['save_turn'] = save_turn
 
     def is_block(self, idx, idy):
         block_name = "block%d,%d" % (idx, idy)
@@ -55,17 +55,17 @@ class Serializer(object):
         """Load tiles/objects and generate block object"""
         block_name = "block%d,%d" % (idx, idy)
         block_path = os.path.join(self.serial_path, block_name)
-        block_sh = shelve.open(block_path)
-        block = Block(idx, idy, world=world, tiles=block_sh['tiles'],
-                      entities=block_sh['entities'],
-                      hidden_map=block_sh['hidden_map'],
-                      obstacle_map=block_sh['obstacle_map'],
-                      load_turn=world.turn)
+        with closing(shelve.open(block_path)) as block_sh: 
+            block = Block(idx, idy, world=world, tiles=block_sh['tiles'],
+                          entities=block_sh['entities'],
+                          hidden_map=block_sh['hidden_map'],
+                          obstacle_map=block_sh['obstacle_map'],
+                          load_turn=world.turn)
 
-        save_turn = block_sh['save_turn']
-        # TODO use turndelta maybe
-        turn_delta = world.turn - save_turn
-        block.turn_delta = turn_delta
+            save_turn = block_sh['save_turn']
+            # TODO use turndelta maybe
+            turn_delta = world.turn - save_turn
+            block.turn_delta = turn_delta
 
         return block
 
@@ -75,18 +75,17 @@ class Serializer(object):
         turn = world.turn
         logging.info("saving settings")
         path = os.path.join(self.serial_path, "settings")
-        settings_sh = shelve.open(path)
-        #settings_sh['player'] = player
-        settings_sh['player_x'] = player.x
-        settings_sh['player_y'] = player.y
-        settings_sh['player_index'] = (world.blocks[(Game.idx_cur, Game.idy_cur)]
-                                       .entities[player.x][player.y].index(player))
-        settings_sh['view_x'] = Game.view_x
-        settings_sh['view_y'] = Game.view_y
-        settings_sh['turn'] = turn
-        settings_sh['seed'] = seed
-        logging.info('turn save {}'.format(turn))
-        settings_sh.close()
+        with closing(shelve.open(path)) as settings_sh:
+            #settings_sh['player'] = player
+            settings_sh['player_x'] = player.x
+            settings_sh['player_y'] = player.y
+            settings_sh['player_index'] = (world.blocks[(Game.idx_cur, Game.idy_cur)]
+                                           .entities[player.x][player.y].index(player))
+            settings_sh['view_x'] = Game.view_x
+            settings_sh['view_y'] = Game.view_y
+            settings_sh['turn'] = turn
+            settings_sh['seed'] = seed
+            logging.info('turn save %d', turn)
 
     def load_settings(self):
         """load Game state, player info into dict"""
@@ -94,19 +93,18 @@ class Serializer(object):
         ret_obj = {'player': None, 'game': None}
         if not os.path.exists(path):
             return ret_obj
-        settings_sh = shelve.open(path)
-        ret_obj['player_x'] = settings_sh['player_x']
-        ret_obj['player_y'] = settings_sh['player_y']
-        ret_obj['player_index'] = settings_sh['player_index']
-        Game.view_x = settings_sh['view_x']
-        Game.view_y = settings_sh['view_y'] 
-        Game.update_view()
-        ret_obj['turn'] = settings_sh.get('turn', 0)
-        ret_obj['seed'] = settings_sh.get('seed', None)
-        logging.info('turn load {}'.format(settings_sh.get('turn')))
-        settings_sh.close()
+        with closing(shelve.open(path)) as settings_sh:
+            ret_obj['player_x'] = settings_sh['player_x']
+            ret_obj['player_y'] = settings_sh['player_y']
+            ret_obj['player_index'] = settings_sh['player_index']
+            Game.view_x = settings_sh['view_x']
+            Game.view_y = settings_sh['view_y'] 
+            Game.update_view()
+            ret_obj['turn'] = settings_sh.get('turn', 0)
+            ret_obj['seed'] = settings_sh.get('seed', None)
+            logging.info('turn load %d', settings_sh.get('turn'))
         return ret_obj
-    
+
     def delete_save(self):
         """Permadeath"""
         shutil.rmtree(self.serial_path)
