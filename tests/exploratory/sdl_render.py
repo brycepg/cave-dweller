77da87457e6371055a174389e3990874df67a8b8
Bryce Guinta <azrathud@gmail.com>
2016-03-19 22:04:30 -0600

    sdl_render: remove old ctypes reference

--- tests/exploratory/sdl_render.py
+++ tests/exploratory/sdl_render.py
@@ -9,9 +9,6 @@ from ctypes import *
 
 import draw_text
 
-sdl = CDLL("libSDL-1.2.so.0")
-
-
 def render(surface):
     draw_text.draw_text("pls work", surface, 25, 25)
     #sdl.fill_circle(surface, 1, 1, 5, 5)
@@ -19,7 +16,8 @@ def render(surface):
 
 libtcod.console_set_custom_font(os.path.join('../../fonts', 'dejavu12x12_gs_tc.png'), libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(90,40, 'test', False, libtcod.RENDERER_SDL)
-draw_text.set_font(pt_size=8)
+# Set font size, init ttf
+draw_text.set_font(pt_size=12)
 libtcod.sys_register_SDL_renderer(render)
 libtcod.sys_set_fps(40)
 libtcod.console_flush()
