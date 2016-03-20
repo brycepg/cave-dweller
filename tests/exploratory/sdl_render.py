5b28c2978f5edd2a5a36d9c0eb0d669a653ffb19
Bryce Guinta <azrathud@gmail.com>
2016-03-19 22:03:34 -0600

    Got SDL_render callback POC working

--- tests/exploratory/sdl_render.py
+++ tests/exploratory/sdl_render.py
@@ -1,19 +1,25 @@
-"""As of now sdl surfaces do not work"""
+"""I got sys_register_SDL_renderer to work!"""
 import time
 import sys
 import os
-sys.path.append("cave_dweller")
+sys.path.append("../../cave_dweller")
 
 import libtcodpy as libtcod
-import pygame
-from pygame.locals import *
+from ctypes import *
 
-def render(my_surface):
-    import pdb; pdb.set_trace()
-    pygame.draw.circle(my_surface, pygame.Color(155, 155, 155), (100,100), 100)
+import draw_text
 
-libtcod.console_set_custom_font(os.path.join('fonts', 'dejavu12x12_gs_tc.png'), libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+sdl = CDLL("libSDL-1.2.so.0")
+
+
+def render(surface):
+    draw_text.draw_text("pls work", surface, 25, 25)
+    #sdl.fill_circle(surface, 1, 1, 5, 5)
+
+
+libtcod.console_set_custom_font(os.path.join('../../fonts', 'dejavu12x12_gs_tc.png'), libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 libtcod.console_init_root(90,40, 'test', False, libtcod.RENDERER_SDL)
+draw_text.set_font(pt_size=8)
 libtcod.sys_register_SDL_renderer(render)
 libtcod.sys_set_fps(40)
 libtcod.console_flush()
