121673a9d2819b42ef50e17e6df989387a78f083
Bryce Guinta <azrathud@gmail.com>
2016-03-29 14:47:37 -0600

    test __init__: don't automatically import modules to stop Game() from statically loading

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,7 +1,4 @@
 import sys
 import os
 cave_module = os.path.join(os.path.dirname(os.path.dirname(__file__)), "cave_dweller")
-print(cave_module)
 sys.path.append(cave_module)
-from . import test_player
-from . import test_block
